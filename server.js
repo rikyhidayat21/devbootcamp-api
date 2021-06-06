be7abd1383928b6fab880e8ce85cdf9f50970ca0
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-06-06 11:07:11 +0700

    connecting to database mongodb

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/logger");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+// load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// connect to database
+connectDB();
 
 // route files
 const bootcamps = require("./routes/bootcamps");
-// load env vars
-dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,6 +23,14 @@ if (process.env.NODE_ENV === "development") {
 // mount router
 app.use("/api/v1/bootcamps", bootcamps);
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
 );
+
+// handle unhandled promise rejections
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server and exit process
+  server.close(() => process.exit(1));
+});
