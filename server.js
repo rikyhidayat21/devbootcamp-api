551d9d92fa0062315b0ae04716c2f457d6636d4d
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-06-06 11:11:46 +0700

    add colors to console

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const colors = require("colors");
 
 // load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -24,13 +25,15 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/bootcamps", bootcamps);
 
 const server = app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
 );
 
 // handle unhandled promise rejections
 
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // close server and exit process
   server.close(() => process.exit(1));
 });
